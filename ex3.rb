f974a1cc8844281dd311eb470197cb31c2729647
Stefan Piatkov <stefanpiatkov@work.ait-sydney.net.au>
2017-03-29 10:00:13 +1100

    first commit

---
+++ ex3.rb
@@ -0,0 +1,32 @@
+
+# outputs what is written.
+puts "I will now count my chickens:"
+
+# counts the numbers in the brackets (general math).
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# outputs what is written.
+puts "Now I will now count the eggs:"
+
+# counts the eggs
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Asking if the next statement is true or false
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Is 3=2 less than 5-7? Gives true or false value.
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# Counts the numbers as above, shows you why the value is false.
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "Whats is 5 - 7? #{5.0 - 7.0}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+# true or false statements
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
